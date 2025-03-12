ff90c0c3b0e4e6ceab09a9b71b6bb758167a116a
Lyco Lacuesta <lllacuesta@up.edu.ph>
2025-03-12 11:01:15 +0800

    feat: add seeds for data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+# create companies
+Company.destroy_all # remove contents for dev only
+Company.create([
+  {
+    company_name: "Aperture Science"
+  }
+])
+p "Created 1 company"
+
+Project.destroy_all
+Project.create([
+  {
+    project_name: "Portal Gun Device",
+    duration: rand(24..72),
+    start_date: Date.today,
+    company_id: 1
+  },
+  {
+    project_name: "Aerial Faith Plate",
+    duration: rand(24..72),
+    start_date: Date.today,
+    company_id: 1
+  }
+])
+p "Created 2 projects"
+
+Employee.destroy_all
+(10).times do |i|
+  Employee.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    company_id: 1
+  )
+end
+p "Created 10 employees"
