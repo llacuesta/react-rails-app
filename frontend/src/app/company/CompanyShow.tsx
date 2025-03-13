46d588b4647f2f994f7debcc7183c86beb6b0fc1
Lyco Lacuesta <lllacuesta@up.edu.ph>
2025-03-13 14:12:18 +0800

    feat: add project information to company

--- frontend/src/app/company/CompanyShow.tsx
+++ frontend/src/app/company/CompanyShow.tsx
@@ -4,17 +4,21 @@ import { useEffect, useState } from "react";
 import { Company } from "./types/Company";
 import { Employee } from "../employee/types/Employee";
 import { useEmployees } from "../employee/hooks/useEmployee";
+import { useProjects } from "../project/hooks/useProject";
+import { Project } from "../project/types/Project";
 
 export default function CompanyShow() {
     // api calls
     const param = useParams() as { company_id: string }
     const { data: company, error, isLoading } = useCompany(param.company_id);
     const { data: employees, error: empError, isLoading: empIsLoading } = useEmployees();
+    const { data: projects, error: projError, isLoading: projisLoading } = useProjects();
 
     // states
     const [toggleEdit, setToggleEdit] = useState(false);
     const [newCompanyName, setNewCompanyName] = useState("");
     const [companyEmployees, setCompanyEmployees] = useState(Array<Employee>);
+    const [companyProjects, setCompanyProjects] = useState(Array<Project>);
     useEffect(() => {
       if (company) {
         setNewCompanyName(company.data.company_name);
@@ -26,6 +30,12 @@ export default function CompanyShow() {
         setCompanyEmployees(filteredEmployees);
       }
     }, [employees, param.company_id]);
+    useEffect(() => {
+      if (projects) {
+        const filteredProjects = projects.data.filter(project => project.company_id === parseInt(param.company_id));
+        setCompanyProjects(filteredProjects);
+      }
+    }, [projects, param.company_id]);
 
     // save edit
     const updateMutation = useUpdateCompany();
@@ -96,6 +106,7 @@ export default function CompanyShow() {
                   <p className="text-lg text-red-500">An error has occured</p>
                   <p>{empError.message}</p>
                 </div> :
+                companyEmployees.length === 0 ? <p className="text-lg">This company has no employees.</p> :
                 companyEmployees.map(employee => (
                   <div className="inline-flex gap-2 p-2 px-4 border-b border-sky-400 w-full">
                     <p className="w-[30px]">{employee.id}</p> <p>{employee.first_name}</p> <p>{employee.last_name}</p>
@@ -106,16 +117,29 @@ export default function CompanyShow() {
           </div>
 
           {/* list of projects */}
-          <div className="flex flex-col w-1/2 items-start">
+          <div className="flex flex-col w-1/2 gap-4 items-start">
             <div className="flex gap-4 items-end">
               <p className="text-3xl font-bold">Projects</p>
               <Link to="/projects">
                 <button className="hover:underline">View all projects</button>
               </Link>
             </div>
-            {
-              
-            }
+            
+            <div className="flex flex-col w-full items-start">
+              {
+                projisLoading ? <p className="text-lg">Fetching project information...</p> :
+                projError ? <div>
+                  <p className="text-lg text-red-500">An error has occured</p>
+                  <p>{projError.message}</p>
+                </div> :
+                companyProjects.length === 0 ? <p className="text-lg">This company has no projects.</p> :
+                companyProjects.map(project => (
+                  <div className="inline-flex gap-2 p-2 px-4 border-b border-sky-400 w-full">
+                    <p className="w-[30px]">{project.id}</p> <p>{project.project_name}</p>
+                  </div>
+                ))
+              }              
+            </div>
           </div>
         </div>
       </div>
